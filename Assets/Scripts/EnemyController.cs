2d53e73ca166907ed3e1cb7bdad1f0353e3400d8
jaakop <jaakko@kronogra.fi>
2023-02-04 00:53:30 +0200

    Added enemy controller and made enemies move

--- Assets/Scripts/EnemyController.cs
+++ Assets/Scripts/EnemyController.cs
@@ -2,20 +2,49 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class EnemyController : MonoBehaviour
+public class enemyController : MonoBehaviour
 {
-    private Transform playerTransform;
+
+    public List<Transform> trees = new List<Transform>();
+
+    [SerializeField]
+    private List<Transform> enemies = new List<Transform>();
+
+    [SerializeField]
+    private float enemySpeed = 1f;
 
     // Start is called before the first frame update
     void Start()
     {
-        playerTransform = GameObject.Find("Player").transform;
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.LookAt(playerTransform);
-        transform.eulerAngles = new Vector3 (0, transform.eulerAngles.y, transform.eulerAngles.z);  
+        MoveEnemies();
+    }
+
+    private void MoveEnemies()
+    {
+        foreach (var enemy in enemies)
+        {
+            Transform closest = trees[0];
+            float closestDistance = Vector3.Distance(trees[0].position, enemy.position);
+
+            for (int i = 1; i < trees.Count; i++)
+            {
+                if(Vector3.Distance(trees[i].position, enemy.position) < closestDistance)
+                {
+                    closest = trees[i];
+                    closestDistance = Vector3.Distance(trees[i].position, enemy.position);
+                }
+            }
+
+            if (closestDistance > 0.1f)
+            {
+                enemy.position = Vector3.Lerp(enemy.position, closest.position, enemySpeed * 0.0001f);
+            }
+        }
     }
 }

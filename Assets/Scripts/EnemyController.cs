5c9a9a135132b5dc914197a4564c2f00a6a8742b
jaakop <jaakko@kronogra.fi>
2023-02-04 00:06:29 +0200

    Added enemy base

---
+++ Assets/Scripts/EnemyController.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyController : MonoBehaviour
+{
+    private Transform playerTransform;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerTransform = GameObject.Find("Player").transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.LookAt(playerTransform);
+        transform.eulerAngles = new Vector3 (0, transform.eulerAngles.y, transform.eulerAngles.z);  
+    }
+}

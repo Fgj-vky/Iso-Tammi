aa015576d18149938e74a1b05d456a6d30ce56a7
jaakop <jaakko@kronogra.fi>
2023-02-04 21:21:45 +0200

    Lose

--- Assets/Scripts/mainTreeScript.cs
+++ Assets/Scripts/mainTreeScript.cs
@@ -10,12 +10,18 @@ public class mainTreeScript : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        GameObject.FindGameObjectWithTag("GameController").GetComponent<gameController>().AddTree(gameObject);
     }
 
     // Update is called once per frame
     void Update()
     {
         transform.localScale *= (1 + scaleFactor * Time.deltaTime);
+    }
+
+    private void OnDestroy()
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene("MainMenu");
+        UnityEngine.SceneManagement.SceneManager.UnloadSceneAsync("testing-scene");
     }
 }
